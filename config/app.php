2cb5c87a7ce452b92b612d2a7c47229a5556c66e
Ollie <oliver.siket@gmail.com>
2019-09-20 12:58:04 +0100

    debug class added app.php

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
          * Package Service Providers...
          */
         Barryvdh\Cors\ServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
         /*
          * Application Service Providers...
          */
